16dd3d36282df803ca5d43fe392894dd74bb73e1
SS-Carlos-Alfonso <87091215+SS-Carlos-Alfonso@users.noreply.github.com>
2021-07-19 13:40:21 -0400

    Update commands.sql

--- src/RDBMS/commands.sql
+++ src/RDBMS/commands.sql
@@ -46,7 +46,7 @@ HAVING COUNT(*) > 5
 /*Question 7
 SELECT title, noOfCopies
 FROM ( ( (tbl_author NATURAL JOIN tbl_book)
-NATURAL JOIN tbl_book_copies)
-NATURAL JOIN tbl_library_branch)
+INNER JOIN tbl_book_copies)
+INNER JOIN tbl_library_branch)
 WHERE authorName = 'Stephen King' and branchName = 'Central'
 */
